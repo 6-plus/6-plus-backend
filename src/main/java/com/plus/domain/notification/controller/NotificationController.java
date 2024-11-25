d87ea6b6d8ebd848fc8d21573bd259e7a1690e29
kimwanyoung <dhks2869@gmail.com>
2024-11-25 21:39:10 +0900

    [#13] refactor : 테스트 용 코드 제거

--- src/main/java/com/plus/domain/notification/controller/NotificationController.java
+++ src/main/java/com/plus/domain/notification/controller/NotificationController.java
@@ -5,13 +5,11 @@ import java.io.IOException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import com.plus.domain.draw.entity.Draw;
 import com.plus.domain.notification.service.NotificationService;
 
 import lombok.RequiredArgsConstructor;
@@ -28,10 +26,5 @@ public class NotificationController {
 		return ResponseEntity
 			.status(HttpStatus.OK)
 			.body(notificationService.connect(userId));
-	}
-
-	@PostMapping
-	public void test() {
-		notificationService.addTask(Draw.builder().build());
 	}
 }
--- src/main/java/com/plus/domain/notification/service/NotificationService.java
+++ src/main/java/com/plus/domain/notification/service/NotificationService.java
@@ -2,7 +2,6 @@ package com.plus.domain.notification.service;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.scheduling.TaskScheduler;
@@ -12,7 +11,6 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.plus.domain.common.SchedulerTimeUtil;
 import com.plus.domain.draw.entity.Draw;
-import com.plus.domain.draw.entity.Product;
 import com.plus.domain.notification.entity.Notification;
 import com.plus.domain.notification.entity.SseEmitters;
 import com.plus.domain.notification.enums.DrawNotificationType;
@@ -29,24 +27,13 @@ public class NotificationService {
 	private final SseEmitters emitters;
 	private final TaskScheduler taskScheduler;
 	private final NotificationRepository notificationRepository;
-	private static final Integer ONE_HOUR = 1;
 
 	public SseEmitter connect(Long userId) throws IOException {
 		return emitters.add(userId);
 	}
 
 	@Transactional
-	public void addTask(Draw draw1) {
-		Draw draw = Draw.builder()
-			.id(1L)
-			.startTime(LocalDateTime.now().plusSeconds(3610))
-			.endTime(LocalDateTime.now().plusSeconds(3620))
-			.product(Product.builder()
-				.productName("애플워치")
-				.productImage("image")
-				.productDescription("desc")
-				.build())
-			.build();
+	public void addTask(Draw draw) {
 		for (DrawNotificationType type : DrawNotificationType.values()) {
 			try {
 				Instant notificationTime = SchedulerTimeUtil.calculateNotificationTime(draw, type);
@@ -66,7 +53,8 @@ public class NotificationService {
 			Notification notification = notificationRepository.findByDrawIdAndType(draw.getId(), type)
 				.orElseThrow(() -> new IllegalStateException("존재하지 않는 응모입니다."));
 			String productName = draw.getProduct().getProductName();
-			List<Long> ids = List.of(1L, 2L); // TODO: 실제 repository에서 해당 응모를 관심 추천 한 userIds 조회 필요
+			// TODO: 실제 repository에서 해당 응모를 관심 추천 한 userIds 조회 필요
+			List<Long> ids = List.of(1L, 2L);
 			emitters.send(ids, productName + type.message);
 			notification.complete();
 			notificationRepository.save(notification);
