d98ef1ecc9ae0a3c3e4059ead744d65dd2970eae
kimwanyoung <dhks2869@gmail.com>
2024-11-25 14:07:48 +0900

    [#7] refactor : AtomicLong으로 생성하던 userId RequestParam으로 받아오도록 수정

--- src/main/java/com/plus/domain/notification/controller/NotificationController.java
+++ src/main/java/com/plus/domain/notification/controller/NotificationController.java
@@ -1,12 +1,12 @@
 package com.plus.domain.notification.controller;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -20,12 +20,9 @@ import lombok.RequiredArgsConstructor;
 public class NotificationController {
 
 	private final NotificationService notificationService;
-	private final AtomicLong userIds = new AtomicLong();
 
 	@GetMapping("/connect")
-	public ResponseEntity<SseEmitter> connect() throws IOException {
-		// TODO: 추후 AuthUser에 존재하는 실제 userId로 교체 예정
-		long userId = userIds.getAndIncrement();
+	public ResponseEntity<SseEmitter> connect(@RequestParam Long userId) throws IOException {
 		return ResponseEntity
 			.status(HttpStatus.OK)
 			.body(notificationService.connect(userId));
