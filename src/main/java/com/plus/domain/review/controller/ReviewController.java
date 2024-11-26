2faa54f33daff1d093f5ccdee4f7404b97bc03fb
inzulmi <inzulmi132@gmail.com>
2024-11-26 09:23:50 +0900

    [#23] feat: 리뷰 작성 기능 구현

--- src/main/java/com/plus/domain/review/controller/ReviewController.java
+++ src/main/java/com/plus/domain/review/controller/ReviewController.java
@@ -1,16 +1,35 @@
 package com.plus.domain.review.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.plus.domain.review.dto.request.ReviewSaveRequestDto;
+import com.plus.domain.review.dto.response.ReviewSaveResponseDto;
 import com.plus.domain.review.service.ReviewService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/draws/{drawId}/reviews")
+@RequestMapping("/api/reviews")
 public class ReviewController {
 
 	private final ReviewService reviewService;
+
+	@PostMapping("/draws/{drawId}")
+	public ResponseEntity<ReviewSaveResponseDto> saveReview(
+		@RequestBody ReviewSaveRequestDto requestDto,
+		@PathVariable(name = "drawId") Long drawId,
+		@RequestParam Long userId
+	) {
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(reviewService.saveReview(requestDto, drawId, userId));
+	}
 }
--- src/main/java/com/plus/domain/review/service/ReviewService.java
+++ src/main/java/com/plus/domain/review/service/ReviewService.java
@@ -2,6 +2,9 @@ package com.plus.domain.review.service;
 
 import org.springframework.stereotype.Service;
 
+import com.plus.domain.review.dto.request.ReviewSaveRequestDto;
+import com.plus.domain.review.dto.response.ReviewSaveResponseDto;
+import com.plus.domain.review.entity.Review;
 import com.plus.domain.review.repository.ReviewRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +14,13 @@ import lombok.RequiredArgsConstructor;
 public class ReviewService {
 
 	private final ReviewRepository reviewRepository;
+
+	public ReviewSaveResponseDto saveReview(ReviewSaveRequestDto requestDto, Long drawId, Long userId) {
+		String contents = requestDto.getContents();
+		String image = requestDto.getImage();
+		Review review = reviewRepository.save(
+			Review.builder().contents(contents).image(image).userId(userId).drawId(drawId).build()
+		);
+		return new ReviewSaveResponseDto(review);
+	}
 }
