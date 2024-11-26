9120abf123fd510d84be555541aa8089f42db00f
kimwanyoung <dhks2869@gmail.com>
2024-11-26 11:34:22 +0900

    [#34] refactor : 각 도메인 exception 주석 추가

--- src/main/java/com/plus/domain/common/exception/enums/ExceptionCode.java
+++ src/main/java/com/plus/domain/common/exception/enums/ExceptionCode.java
@@ -8,10 +8,20 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ExceptionCode {
-	// 알림 코드
+	// Notification Exception
 	NOT_FOUND_NOTIFICATION(HttpStatus.NOT_FOUND, "존재하지 않는 알림입니다."),
-	SSE_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SSE 연결 중 오류 발생");
+	SSE_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SSE 연결 중 오류 발생"),
+	// Draw Exception
 
+	// Auth Exception
+
+	// Review Exception
+
+	// User Exception
+
+	// Favorite Exception
+	
+	;
 	private final HttpStatus code;
 	private final String message;
 }
