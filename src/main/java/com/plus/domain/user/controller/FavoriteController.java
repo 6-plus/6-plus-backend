2f630de46b52fedf1f4153160116b16e20896a82
gebeen-dev <rrdd1207@gmail.com>
2024-11-26 20:15:31 +0900

    [#52] refactor: FavoriteController

--- src/main/java/com/plus/domain/user/controller/FavoriteController.java
+++ src/main/java/com/plus/domain/user/controller/FavoriteController.java
@@ -17,35 +17,32 @@ import java.util.List;
 @RequestMapping("/api/draws")
 public class FavoriteController {
 
-    private final FavoriteService favoriteService;
-    private final DrawService drawService;
+	private final FavoriteService favoriteService;
+	private final DrawService drawService;
 
 
-    @PostMapping("/{drawId}/favorites")
-    public ResponseEntity<FavoriteSaveResponseDto> saveFavorite(
-            @PathVariable(name = "drawId") Long drawId) {
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(favoriteService.saveFavorite(drawId, 1L));  // TODO : 나중에 토큰에서 가져와서 넣을 예정
-    }
+	@PostMapping("/{drawId}/favorites")
+	public ResponseEntity<FavoriteSaveResponseDto> saveFavorite(
+		@PathVariable(name = "drawId") Long drawId) {
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(favoriteService.saveFavorite(drawId, 1L));  // TODO : 나중에 토큰에서 가져와서 넣을 예정
+	}
 
-    //나의관심응모조회
-    @GetMapping("/favorites/my")
-    public ResponseEntity<List<FavoriteSearchResponseDto>> searchFavorite(
-            @RequestParam Long userId) {
-
-        List<FavoriteSearchResponseDto> res = favoriteService.searchfavorite(userId);
-
-        return ResponseEntity.ok(res);
-    }
+	//나의관심응모조회
+	@GetMapping("/favorites/my")
+	public ResponseEntity<List<FavoriteSearchResponseDto>> searchFavorite(
+		@RequestParam Long userId) {
+		List<FavoriteSearchResponseDto> res = favoriteService.searchfavorite(userId);
+		return ResponseEntity.ok(res);
+	}
 
 
-    @DeleteMapping("{drawId}/favorites/{favoriteId}")
-    public ResponseEntity<FavoriteDeleteResponseDto> deleteFavorite(
-            @PathVariable(name = "drawId") Long drawId,
-            @PathVariable(name = "favoriteId") Long favoriteId) {
-        FavoriteDeleteResponseDto res = favoriteService.deleteFavorite(favoriteId);
-
-        return ResponseEntity.ok(res);
-    }
+	@DeleteMapping("{drawId}/favorites/{favoriteId}")
+	public ResponseEntity<FavoriteDeleteResponseDto> deleteFavorite(
+		@PathVariable(name = "drawId") Long drawId,
+		@PathVariable(name = "favoriteId") Long favoriteId) {
+		FavoriteDeleteResponseDto res = favoriteService.deleteFavorite(favoriteId);
+		return ResponseEntity.ok(res);
+	}
 }
