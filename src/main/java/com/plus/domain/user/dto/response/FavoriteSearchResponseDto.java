ba20faa1f6841576d8028e3a71aa23b102ef0845
gebeen-dev <rrdd1207@gmail.com>
2024-11-26 22:21:54 +0900

    [#52] fix : 충돌 해결

--- src/main/java/com/plus/domain/user/dto/response/FavoriteSearchResponseDto.java
+++ src/main/java/com/plus/domain/user/dto/response/FavoriteSearchResponseDto.java
@@ -5,30 +5,33 @@ import com.plus.domain.draw.entity.Product;
 import com.plus.domain.draw.enums.DrawType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class FavoriteSearchResponseDto {
 
-    private Long id;
-    private Integer totalWinners;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private LocalDateTime resultTime;
-    private DrawType type;
-    private Product product;
+	private Long id;
+	private Integer totalWinners;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+	private LocalDateTime resultTime;
+	private DrawType type;
+	private Product product;
 
-    public static FavoriteSearchResponseDto from(Draw draw) {
-        return FavoriteSearchResponseDto.builder()
-                .id(draw.getId())
-                .totalWinners(draw.getTotalWinner())
-                .startTime(draw.getStartTime())
-                .endTime(draw.getEndTime())
-                .resultTime(draw.getResultTime())
-                .type(draw.getDrawType())
-                .product(draw.getProduct())
-                .build();
-    }
+	public static FavoriteSearchResponseDto from(Draw draw) {
+		return FavoriteSearchResponseDto.builder()
+			.id(draw.getId())
+			.totalWinners(draw.getTotalWinner())
+			.startTime(draw.getStartTime())
+			.endTime(draw.getEndTime())
+			.resultTime(draw.getResultTime())
+			.type(draw.getDrawType())
+			.product(draw.getProduct())
+			.build();
+	}
 }
