b971b6d9734afdcf1566b84d6394004556e3d71a
inzulmi <inzulmi132@gmail.com>
2024-11-26 09:49:13 +0900

    [#23] feat: 응모 당첨 리뷰 조회 기능 구현

--- src/main/java/com/plus/domain/review/controller/ReviewController.java
+++ src/main/java/com/plus/domain/review/controller/ReviewController.java
@@ -1,7 +1,11 @@
 package com.plus.domain.review.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.plus.domain.review.dto.request.ReviewSaveRequestDto;
 import com.plus.domain.review.dto.response.ReviewSaveResponseDto;
+import com.plus.domain.review.dto.response.ReviewSearchResponseDto;
 import com.plus.domain.review.service.ReviewService;
 
 import lombok.RequiredArgsConstructor;
@@ -31,5 +36,15 @@ public class ReviewController {
 		return ResponseEntity
 			.status(HttpStatus.CREATED)
 			.body(reviewService.saveReview(requestDto, drawId, userId));
+	}
+
+	@GetMapping("/draws/{drawId}")
+	public ResponseEntity<Page<ReviewSearchResponseDto>> searchReview(
+		@PathVariable(name = "drawId") Long drawId,
+		@PageableDefault Pageable pageable
+	) {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(reviewService.searchReview(drawId, pageable));
 	}
 }
--- src/main/java/com/plus/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/plus/domain/review/repository/ReviewRepository.java
@@ -1,8 +1,11 @@
 package com.plus.domain.review.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.plus.domain.review.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+	Page<Review> findAllByDrawId(Long drawId, Pageable pageable);
 }
