2a1debf7a8a514b387d063b97c2fea002953fcfb
gebeen-dev <rrdd1207@gmail.com>
2024-11-26 15:41:21 +0900

    [#19] feat : 관심응모 조회 삭제 기능 구현

--- src/main/java/com/plus/domain/user/dto/response/FavoriteSearchResponseDto.java
+++ src/main/java/com/plus/domain/user/dto/response/FavoriteSearchResponseDto.java
@@ -1,11 +1,14 @@
 package com.plus.domain.user.dto.response;
 
+import com.plus.domain.draw.entity.Draw;
 import com.plus.domain.draw.entity.Product;
 import com.plus.domain.draw.enums.DrawType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 @AllArgsConstructor
 public class FavoriteSearchResponseDto {
 
@@ -17,4 +20,15 @@ public class FavoriteSearchResponseDto {
     private DrawType type;
     private Product product;
 
+    public static FavoriteSearchResponseDto from(Draw draw) {
+        return FavoriteSearchResponseDto.builder()
+                .id(draw.getId())
+                .totalWinners(draw.getTotalWinner())
+                .startTime(draw.getStartTime())
+                .endTime(draw.getEndTime())
+                .resultTime(draw.getResultTime())
+                .type(draw.getDrawType())
+                .product(draw.getProduct())
+                .build();
+    }
 }
--- src/main/java/com/plus/domain/user/service/FavoriteService.java
+++ src/main/java/com/plus/domain/user/service/FavoriteService.java
@@ -2,7 +2,9 @@ package com.plus.domain.user.service;
 
 import com.plus.domain.draw.entity.Draw;
 import com.plus.domain.draw.repository.DrawRepository;
-import com.plus.domain.user.dto.FavoriteSaveResponseDto;
+import com.plus.domain.user.dto.response.FavoriteDeleteResponseDto;
+import com.plus.domain.user.dto.response.FavoriteSaveResponseDto;
+import com.plus.domain.user.dto.response.FavoriteSearchResponseDto;
 import com.plus.domain.user.entity.Favorite;
 import com.plus.domain.user.entity.User;
 import com.plus.domain.user.repository.FavoriteRepository;
@@ -10,6 +12,9 @@ import com.plus.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,12 +25,12 @@ public class FavoriteService {
     private final UserRepository userRepository;
 
     @Transactional
-    public FavoriteSaveResponseDto saveFavorite(Long drawId, String email) {
+    public FavoriteSaveResponseDto saveFavorite(Long drawId, Long userId) {
         Draw draw = drawRepository.findById(drawId)
                 .orElseThrow(() -> new IllegalArgumentException("Draw not found with ID: " + drawId));
 
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("User not found with email: " + email));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + userId));
 
         Favorite savedFavorite = favoriteRepository.save(Favorite.builder()
                 .drawId(draw.getId())
@@ -35,12 +40,27 @@ public class FavoriteService {
         return savedFavorite.toDto();
     }
 
+    @Transactional(readOnly = true)
+    public List<FavoriteSearchResponseDto> searchfavorite(Long userId) {
 
-//    //관심 응모 삭제 sercive package
-//    public FavoriteDeleteResponseDto deleteFavorite(Long favoriteId) {
-//        favoriteRepository.deleteById(favoriteId);
-//        return null;
-//
-//
-//    }
+        List<Favorite> res = favoriteRepository.findAllByUserId(userId);
+
+        List<FavoriteSearchResponseDto> responseDtos = new ArrayList<>();
+        for (Favorite re : res) {
+            Draw draw = drawRepository.findById(re.getDrawId()).get();
+            responseDtos.add(FavoriteSearchResponseDto.from(draw));
+        }
+
+        return responseDtos;
+    }
+
+    @Transactional
+    public FavoriteDeleteResponseDto deleteFavorite(Long favoriteId) {
+        favoriteRepository.deleteById(favoriteId);
+        return FavoriteDeleteResponseDto.builder()
+                .message("관심응모가 삭제 되었습니다.")
+                .build();
+
+
+    }
 }
