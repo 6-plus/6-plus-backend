8d63b9a6e63197362c33ec3f03a3a6c3bcee244c
gebeen-dev <rrdd1207@gmail.com>
2024-11-26 22:16:12 +0900

    [#52] feat : Favorite ExceptionCode 생성

--- src/main/java/com/plus/domain/common/exception/enums/ExceptionCode.java
+++ src/main/java/com/plus/domain/common/exception/enums/ExceptionCode.java
@@ -1,9 +1,8 @@
 package com.plus.domain.common.exception.enums;
 
-import org.springframework.http.HttpStatus;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
@@ -15,6 +14,8 @@ public enum ExceptionCode {
 	// Draw Exception
 	NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "권한이 없습니다. 관리자만 응모를 생성할 수 있습니다."),
 	DUPLICATE_DRAW_NAME(HttpStatus.CONFLICT, "이미 등록된 응모 이름입니다."),
+	INVALID_TOTAL_WINNERS(HttpStatus.BAD_REQUEST, "당첨자 수는 1명 이상이어야 합니다."),
+	INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "시작일과 종료일이 올바르지 않습니다."),
 	FILE_NOT_FOUND(HttpStatus.BAD_REQUEST, "업로드할 파일이 존재하지 않습니다."),
 	DRAW_NOT_FOUND(HttpStatus.BAD_REQUEST, "응모가 존재하지 않습니다."),
 	FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일을 삭제할 수 없습니다."),
@@ -27,8 +28,10 @@ public enum ExceptionCode {
 	// User Exception
 
 	// Favorite Exception
+	DRAW_NOT_FOUND_OF_FAVORITE(HttpStatus.NOT_FOUND, "요청한 응모를 찾을 수 없습니다.."),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 사용자를 찾을 수 없습니다."),
+	DUPLICATE_FAVORITE(HttpStatus.NOT_FOUND, "이미 저장된 관심 응모입니다.");
 
-	;
 	private final HttpStatus code;
 	private final String message;
 }
