fea99094494806e4db5698f0eddb3e7f229cf31b
inzulmi <inzulmi132@gmail.com>
2024-11-28 15:57:56 +0900

    rabbitMq init

--- src/main/java/com/plus/PlusApplication.java
+++ src/main/java/com/plus/PlusApplication.java
@@ -2,12 +2,14 @@ package com.plus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+@ConfigurationPropertiesScan
 public class PlusApplication {
 
 	public static void main(String[] args) {
